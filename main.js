8bb6a6979159e9d414ac0c8b30910a530dc47eb9
Alan Compton <112966271+AlanCompton@users.noreply.github.com>
2023-01-23 13:29:03 -0600

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello World!");
-console.log("\n")
-console.log("HH  HH        111 111             WW        WW                111       DD");
-console.log("HH  HH   EEE  111 111   000       WW        WW   000   RRRRR  111   DDD DD");
-console.log("HHHHHH EE   E 111 111 00   00     WW    W   WW 00   00 RR   R 111 DD    DD");
-console.log("HH  HH EEEEE  111 111 00   00     WW  W  W  WW 00   00 RR     111 DD    DD");
-console.log("HH  HH  EEEEE 111 111   000       WWW      WWW   000   RR     111   DDD DD");
